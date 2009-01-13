317fd97062801b0406eaab7b69ca322bec043eba
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-01-13 08:49:48 +0000

    Pump up to fritz_box-labor-7270-13362.zip
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@5 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/sp-to-fritz.sh
+++ trunk/speed-to-fritz/sp-to-fritz.sh
@@ -3,7 +3,7 @@ PATH=/sbin:$PATH
 #dont change names of variables because some of the names are used in other files as well!
 ##########################################################################
 # Date of current version:                                          
-Tag="05"; Monat="01"; Jahr="09"
+Tag="13"; Monat="01"; Jahr="09"
 export SKRIPT_DATE="$Tag.$Monat.$Jahr"
 export SKRIPT_DATE_ISO="$Jahr.$Monat.$Tag"
 export SKRIPT_REVISION="$Jahr$Monat$Tag"
