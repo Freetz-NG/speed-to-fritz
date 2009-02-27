0957b5bf9ea40440c2115ee04586ba1b362a5705
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-02-27 11:58:59 +0000

    20090227-0658 - W920: new releas added FRITZ.Box_Fon_WLAN_7270.54.04.70.image
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@152 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/sp-to-fritz.sh
+++ trunk/speed-to-fritz/sp-to-fritz.sh
@@ -3,7 +3,7 @@ export PATH=$PATH:/sbin
 #dont change names of variables because some of the names are used in other files as well!
 ##########################################################################
 # Date of current version:                                          
-Tag="26"; Monat="02"; Jahr="09"
+Tag="27"; Monat="02"; Jahr="09"
 export SKRIPT_DATE="$Tag.$Monat.$Jahr"
 export SKRIPT_DATE_ISO="$Jahr.$Monat.$Tag"
 export SKRIPT_REVISION="$Jahr$Monat$Tag"
