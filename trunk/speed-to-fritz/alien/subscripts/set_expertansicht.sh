5845c77b6adb13328504284f4f66097d26e9b9d8
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-04-10 07:00:56 +0000

    20090410-0259 - Expert vew enabeled again, is now set via ar7.cfg
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@253 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/alien/subscripts/set_expertansicht.sh
+++ trunk/speed-to-fritz/alien/subscripts/set_expertansicht.sh
@@ -2,29 +2,39 @@
 # include modpatch function
 . ${include_modpatch}
 echo "-- Set expert Ansicht ..."
+
+
  rpl_expert_1()
  {
 	for file in $1; do
 	if [ -f "$file" ]; then
-	 grep -q '<? query box:settings/expertmode/activated ?>' "$file" && echo2 "  'Expert Ansicht set' in File: ${file##*/}"
-	 sed -i -e 's|<? query box:settings/expertmode/activated ?>|1|' "$file"
+	grep -q 'expertmode =' "$file"&& echo2 "  'expertmode removed in file: ${file##*etc}"
+	sed -i -e '/expertmode =/d' "$file" 
+	sed -i -e '/webui ./a\
+expertmode = yes;' "$file"
+	grep -q 'webui .' "$file" ||\
+	sed -i -e '/\/\/ EOF/i\
+webui {\
+expertmode = yes;\
+}' "$file"
+	grep -q 'expertmode =' "$file" && echo2 "  'in file: ${file##*etc}"
 	fi 
 	done
  }
- rpl_expert_1 "$(find "${SRC}/usr/www/${OEMLINK}" -name *.html)" 
- rpl_expert_1 "$(find "${SRC}/usr/www/${OEMLINK}" -name *.js)" 
+
+rpl_expert_1 "$(find "${SRC}/etc" -name ar7.cfg)" 
 
 
-FILELIST="/html/de/system/sitemap.html \
-/html/de/home/sitemap.html \
-/html/de/menus/menu2_system.html"
+#FILELIST="/html/de/system/sitemap.html \
+#/html/de/home/sitemap.html \
+#/html/de/menus/menu2_system.html"
 
-for FILE in $FILELIST; do
-  if [ -f "${SRC}/usr/www/${OEMLINK}/$FILE" ]; then 
-    sed -i -e "/'extended'/d" "${SRC}/usr/www/${OEMLINK}/$FILE"
-    echo2 "  /usr/www/${OEMLINK}/$FILE"
-  fi
-done
+#for FILE in $FILELIST; do
+#  if [ -f "${SRC}/usr/www/${OEMLINK}/$FILE" ]; then 
+#    sed -i -e "/'extended'/d" "${SRC}/usr/www/${OEMLINK}/$FILE"
+#    echo2 "  /usr/www/${OEMLINK}/$FILE"
+#  fi
+#done
 
 
 
