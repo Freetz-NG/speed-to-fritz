76f12824ee782d7d3f0097a8ae81fdc304d3eea1
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-04-15 20:16:44 +0000

    20090415-1620 - W920: Wrapper option for dsld added, but not tested!
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@254 fc5b469a-3574-4ed2-a3ea-762ab7856fab

---
+++ trunk/speed-to-fritz/alien/subscripts/wrap_dsld.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "-- Wrap dsld ..."
+
+mkdir ${SRC}/lib/dsld
+
+cat <<EOF > ${SRC}/lib/dsld/dsld-wrapper.sh
+#!/bin/sh
+# dsld-wrapper.sh
+DSLD_DIR=/lib/dsld
+PATH="\$DSLD_DIR:\$PATH"
+export PATH
+LD_LIBRARY_PATH="\$DSLD_DIR:\$LD_LIBRARY_PATH"
+LD_LIBRARY_PATH="\${LD_LIBRARY_PATH%:}"
+export LD_LIBRARY_PATH
+exec dsld "\$@"
+EOF
+chmod +x ${SRC}/lib/dsld/dsld-wrapper.sh
+
+cp -fdrp "${DST}"/sbin/dsld --target-directory="${SRC}"/lib/dsld
+cp -fdrp "${DST}"/lib/libar7cfg.so.1.0.0 --target-directory="${SRC}"/lib/dsld
+ln -s /lib/dsld/libar7cfg.so.1.0.0 "${SRC}/lib/dsld/libar7cfg.so.1"
+ln -s /lib/dsld/libar7cfg.so.1.0.0 "${SRC}/lib/dsld/libar7cfg.so"
+rm -f "${SRC}/sbin/dsld"
+ln -s /lib/dsld/dsld-wrapper.sh "${SRC}/sbin/dsld"
+exit 0
+
+/lib/dsld/dsld
+/lib/dsld/libar7cfg.so
+/lib/dsld/libar7cfg.so.1.0.0 
+/lib/dsld/libar7cfg.so.1 ( Symlink nach libar7cfg.so.1.0.0 )
+/lib/dsld/libar7cfg.so (Symlink nach libar7cfg.so.1 )
+/lib/dsld/dsld-wrapper.sh
+/sbin/dsld (Symlink nach ../lib/dsld/dsld-wrapper.sh )
