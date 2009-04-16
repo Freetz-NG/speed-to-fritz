1317aa59e6bae75f2d0457b96bd7747a200fdcad
jo-ju <jo-ju@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-04-16 05:23:54 +0000

    * quick fix for http://www.ip-phone-forum.de/showpost.php?p=1313871&postcount=691  * wrapper beautifying
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@255 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/alien/subscripts/wrap_dsld.sh
+++ trunk/speed-to-fritz/alien/subscripts/wrap_dsld.sh
@@ -7,12 +7,10 @@ cat <<EOF > ${SRC}/lib/dsld/dsld-wrapper.sh
 #!/bin/sh
 # dsld-wrapper.sh
 DSLD_DIR=/lib/dsld
-PATH="\$DSLD_DIR:\$PATH"
-export PATH
 LD_LIBRARY_PATH="\$DSLD_DIR:\$LD_LIBRARY_PATH"
 LD_LIBRARY_PATH="\${LD_LIBRARY_PATH%:}"
 export LD_LIBRARY_PATH
-exec dsld "\$@"
+exec \$DSLD_DIR/dsld "\$@"
 EOF
 chmod +x ${SRC}/lib/dsld/dsld-wrapper.sh
 
@@ -24,10 +22,12 @@ rm -f "${SRC}/sbin/dsld"
 ln -s /lib/dsld/dsld-wrapper.sh "${SRC}/sbin/dsld"
 exit 0
 
-/lib/dsld/dsld
-/lib/dsld/libar7cfg.so
-/lib/dsld/libar7cfg.so.1.0.0 
-/lib/dsld/libar7cfg.so.1 ( Symlink nach libar7cfg.so.1.0.0 )
-/lib/dsld/libar7cfg.so (Symlink nach libar7cfg.so.1 )
-/lib/dsld/dsld-wrapper.sh
-/sbin/dsld (Symlink nach ../lib/dsld/dsld-wrapper.sh )
+# target layout:
+#
+#	 /lib/dsld/dsld
+#	 /lib/dsld/libar7cfg.so
+#	 /lib/dsld/libar7cfg.so.1.0.0 
+#	 /lib/dsld/libar7cfg.so.1 ( Symlink nach libar7cfg.so.1.0.0 )
+#	 /lib/dsld/libar7cfg.so (Symlink nach libar7cfg.so.1 )
+#	 /lib/dsld/dsld-wrapper.sh
+#	 /sbin/dsld (Symlink nach ../lib/dsld/dsld-wrapper.sh )
