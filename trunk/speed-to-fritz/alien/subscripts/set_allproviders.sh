8b5e789ca49fd041e5ba832b7d20a449d86bfb50
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-06-09 09:38:29 +0000

    20090609-0543 - Config.in : removeve some emty tabs and changed lenght of some remark lines
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@328 fc5b469a-3574-4ed2-a3ea-762ab7856fab

---
+++ trunk/speed-to-fritz/alien/subscripts/set_allproviders.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# include modpatch function
+. ${include_modpatch}
+echo "-- Set all provoders ..."
+
+FILELIST="/html/de/internet/authform.html"
+
+for FILE in $FILELIST; do
+  if [ -f "${SRC}/usr/www/${OEMLINK}/$FILE" ]; then 
+    sed -i -e 's/$var:allprovider 1/ 1 1/' "${SRC}/usr/www/${OEMLINK}/$FILE"
+    echo2 "  /usr/www/${OEMLINK}/$FILE"
+  fi
+done
+
+exit 0
