b0523908901aade5c8b8fe51549e8b09f1ef8913
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-06-12 06:45:09 +0000

    20090612-0251 - Set Update modules depend. default again to n, becaus 14464 unses links to modules downloaded with plugins.
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@336 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/alien/subscripts/update-module-deps.sh
+++ trunk/speed-to-fritz/alien/subscripts/update-module-deps.sh
@@ -2,12 +2,15 @@
 
 if [ -x /sbin/depmod ]; then
 	echo "-- Updating module dependency informations"
-	/sbin/depmod -a -b "$1" "$2"
+	/sbin/depmod -b "$1" "$2"
 sed -i -e "s|kernel/|/lib/modules/$2/kernel/|" "${1}/lib/modules/$2/modules.dep"
 sed -i -e "s| kernel/| /lib/modules/$2/kernel/|" "${1}/lib/modules/$2/modules.dep"
 rm -f "${1}/lib/modules/$2/modules.dep.bin"
 rm -f "${1}/lib/modules/$2/modules.symbols.bin"
 rm -f "${1}/lib/modules/$2/modules.alias.bin"
+#14464 uses links to /var/...
+echo "/lib/modules/$2/kernel/drivers/net/rfcntl/rfcntl.ko: /lib/modules/$2/kernel/drivers/char/audio/avm_audio.ko" >> "${1}/lib/modules/$2/modules.dep"
+echo "/lib/modules/$2/kernel/drivers/char/audio/avm_audio.ko:" >> "${1}/lib/modules/$2/modules.dep"
 fi
 
 
