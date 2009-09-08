eedd3cf280cf156783cac3bf0f6dabdf59c869ea
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-09-08 18:41:58 +0000

    20090908-1504 - clean up host-tools Makefile
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@518 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/tools/make/fakeroot.mk
+++ trunk/speed-to-fritz/tools/make/fakeroot.mk
@@ -1,21 +1,26 @@
-#this file uses diffent paths as the compareable freetz version
 FAKEROOT_VERSION:=1.12.4
 FAKEROOT_SOURCE:=fakeroot_$(FAKEROOT_VERSION).tar.gz
 FAKEROOT_SOURCE_MD5:=aaefede2405a40c87438e7e833d69b70
 FAKEROOT_SITE:=http://ftp.debian.org/debian/pool/main/f/fakeroot
+#this file uses diffent paths as the compareable freetz version
+#--> redefined in sp-to-fritz.sh, freetz uses /fakeroot instead of /usr
+FAKEROOT_NAME:=fakeroot
+FAKEROOT_DESTDIR:=$(TOOLS_DIR)/usr
+FAKEROOT_BIN_DIR:=$(FAKEROOT_DESTDIR)/bin
+FAKEROOT_LIB_DIR:=$(FAKEROOT_DESTDIR)/lib
+FAKEROOT_TOOL:=$(FAKEROOT_BIN_DIR)/$(FAKEROOT_NAME)
+FAKEROOT_TARGET_SCRIPT:=$(FAKEROOT_BIN_DIR)/$(FAKEROOT_NAME)
+#<--
 FAKEROOT_DIR:=$(TOOLS_SOURCE_DIR)/fakeroot-$(FAKEROOT_VERSION)
 FAKEROOT_MAKE_DIR:=$(TOOLS_DIR)/make
-FAKEROOT_DESTDIR:=$(shell pwd)/$(TOOLS_DIR)
-
 
 $(DL_DIR)/$(FAKEROOT_SOURCE): | $(DL_DIR)
 	$(DL_TOOL) $(DL_DIR) $(TOOLS_DOT_CONFIG) $(FAKEROOT_SOURCE) $(FAKEROOT_SITE) $(FAKEROOT_SOURCE_MD5)
 
 fakeroot-source: $(DL_DIR)/$(FAKEROOT_SOURCE)
 
-$(FAKEROOT_DIR)/.unpacked: $(DL_DIR)/$(FAKEROOT_SOURCE)
-	@rm -rf $(FAKEROOT_DIR) && mkdir -p $(FAKEROOT_DIR)
-	tar -C $(SOURCE_DIR) $(VERBOSE) -xzf $(DL_DIR)/$(FAKEROOT_SOURCE)
+$(FAKEROOT_DIR)/.unpacked: $(DL_DIR)/$(FAKEROOT_SOURCE) | $(TOOLS_SOURCE_DIR)
+	tar -C $(TOOLS_SOURCE_DIR) $(VERBOSE) -xzf $(DL_DIR)/$(FAKEROOT_SOURCE)
 	$(SED) -i "s,getopt --version,getopt --version 2>/dev/null," \
 		$(FAKEROOT_DIR)/scripts/fakeroot.in
 	for i in $(FAKEROOT_MAKE_DIR)/patches/*.fakeroot.patch; do \
@@ -28,7 +33,7 @@ $(FAKEROOT_DIR)/.configured: $(FAKEROOT_DIR)/.unpacked
 		CFLAGS="-O3 -Wall" \
 		CC="$(TOOLS_CC)" \
 		./configure \
-		--prefix=/usr \
+		--prefix=/ \
 		--enable-shared \
 		$(DISABLE_NLS) \
 	);
@@ -37,13 +42,13 @@ $(FAKEROOT_DIR)/.configured: $(FAKEROOT_DIR)/.unpacked
 $(FAKEROOT_DIR)/faked: $(FAKEROOT_DIR)/.configured
 	$(MAKE) -C $(FAKEROOT_DIR)
 
-$(TOOLS_DIR)/usr/bin/fakeroot: $(FAKEROOT_DIR)/faked
+$(FAKEROOT_TARGET_SCRIPT): $(FAKEROOT_DIR)/faked
 	$(MAKE) DESTDIR=$(FAKEROOT_DESTDIR) -C $(FAKEROOT_DIR) install
-	$(SED) -i -e 's,^PREFIX=.*,PREFIX=$(FAKEROOT_DESTDIR)/usr,g' $(TOOLS_DIR)/usr/bin/fakeroot
-	$(SED) -i -e 's,^BINDIR=.*,BINDIR=$(FAKEROOT_DESTDIR)/usr/bin,g' $(TOOLS_DIR)/usr/bin/fakeroot
-	$(SED) -i -e 's,^PATHS=.*,PATHS=$(FAKEROOT_DESTDIR)/usr/lib,g' $(TOOLS_DIR)/usr/bin/fakeroot
+	$(SED) -i -e 's,^PREFIX=.*,PREFIX=$(FAKEROOT_DESTDIR)/,g' $(FAKEROOT_TARGET_SCRIPT)
+	$(SED) -i -e 's,^BINDIR=.*,BINDIR=$(FAKEROOT_DESTDIR)/bin,g' $(FAKEROOT_TARGET_SCRIPT)
+	$(SED) -i -e 's,^PATHS=.*,PATHS=$(FAKEROOT_DESTDIR)/lib,g' $(FAKEROOT_TARGET_SCRIPT)
 
-fakeroot: $(TOOLS_DIR)/usr/bin/fakeroot
+fakeroot: $(FAKEROOT_TARGET_SCRIPT)
 
 fakeroot-clean:
 	$(MAKE) -C $(FAKEROOT_DIR) clean
@@ -52,4 +57,4 @@ fakeroot-dirclean:
 	$(RM) -r $(FAKEROOT_DIR)
 
 fakeroot-distclean:
-	$(RM) -r $(TOOLS_DIR)/usr
+	$(RM) -r $(FAKEROOT_DESTDIR)
