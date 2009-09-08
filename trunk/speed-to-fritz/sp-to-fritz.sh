8c0c1e321c884e07c4bec946de77b52c8a0a702f
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-09-08 20:23:35 +0000

    20090908-1645 - cleanup host-tools makefile again.
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@520 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/sp-to-fritz.sh
+++ trunk/speed-to-fritz/sp-to-fritz.sh
@@ -75,10 +75,13 @@ export MKSQUASHFS_TOOL="mksquashfs-lzma"
 export MKSQUASHFS_OPTIONS="-le -noappend -all-root -info"
 export MKSQUASHFS="${TOOLS_DIR}/${MKSQUASHFS_TOOL}"
 export FAKEROOT_NAME="fakeroot"
-export FAKEROOT_TOOL="${TOOLS_DIR}/usr/bin/${FAKEROOT_NAME}"
-sed -i -e "s|^PREFIX=.*$|PREFIX=${TOOLS_DIR}/usr|g" ${FAKEROOT_TOOL}
-sed -i -e "s|^BINDIR=.*$|BINDIR=${TOOLS_DIR}/usr/bin|g" ${FAKEROOT_TOOL}
-sed -i -e "s|^PATHS=.*$|PATHS=${TOOLS_DIR}/usr/lib|g" ${FAKEROOT_TOOL}
+export FAKEROOT_DESTDIR="${TOOLS_DIR}/usr"
+export FAKEROOT_BIN_DIR="${FAKEROOT_DESTDIR}/bin"
+export FAKEROOT_LIB_DIR="${FAKEROOT_DESTDIR}/lib"
+export FAKEROOT_TOOL="${FAKEROOT_BIN_DIR}/${FAKEROOT_NAME}"
+sed -i -e "s|^PREFIX=.*$|PREFIX=${FAKEROOT_DESTDIR}|g" ${FAKEROOT_TOOL}
+sed -i -e "s|^BINDIR=.*$|BINDIR=${FAKEROOT_BIN_DIR}|g" ${FAKEROOT_TOOL}
+sed -i -e "s|^PATHS=.*$|PATHS=${FAKEROOT_LIB_DIR}|g" ${FAKEROOT_TOOL}
 export TICHKSUM_TOOL="tichksum"
 export TICHKSUM="${TOOLS_DIR}/${TICHKSUM_TOOL}"
 #export TICHKSUM="$TOOLS_DIR/TI-chksum-0.1/tichksum"
