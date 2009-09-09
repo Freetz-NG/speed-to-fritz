b63e0704a80c12288d908453a10959d713032bd4
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-09-09 16:09:52 +0000

    20090909-1236 - fix last few commits, sp-to-fritz.sh is not split any more again, fakeroot is used as with revision 511.
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@530 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/sp-to-fritz.sh
+++ trunk/speed-to-fritz/sp-to-fritz.sh
@@ -74,11 +74,14 @@ export FINDSQUASHFS="${TOOLS_DIR}/${FINDSQUASHFS_TOOL}"
 export MKSQUASHFS_TOOL="mksquashfs-lzma"
 export MKSQUASHFS_OPTIONS="-le -noappend -all-root -info"
 export MKSQUASHFS="${TOOLS_DIR}/${MKSQUASHFS_TOOL}"
-FAKEROOT_TOOL="fakeroot"
-export FAKEROOT="${TOOLS_DIR}/usr/bin/${FAKEROOT_TOOL}"
-sed -i -e "s|^PREFIX=.*$|PREFIX=${TOOLS_DIR}/usr|g" ${FAKEROOT}
-sed -i -e "s|^BINDIR=.*$|BINDIR=${TOOLS_DIR}/usr/bin|g" ${FAKEROOT}
-sed -i -e "s|^PATHS=.*$|PATHS=${TOOLS_DIR}/usr/lib|g" ${FAKEROOT}
+export FAKEROOT_TOOL="fakeroot"
+export FAKEROOT_DESTDIR="${TOOLS_DIR}/usr"
+export FAKEROOT_BIN_DIR="${FAKEROOT_DESTDIR}/bin"
+export FAKEROOT_LIB_DIR="${FAKEROOT_DESTDIR}/lib"
+export FAKEROOT="${FAKEROOT_BIN_DIR}/${FAKEROOT_TOOL}"
+sed -i -e "s|^PREFIX=.*$|PREFIX=${FAKEROOT_DESTDIR}|g" ${FAKEROOT}
+sed -i -e "s|^BINDIR=.*$|BINDIR=${FAKEROOT_BIN_DIR}|g" ${FAKEROOT}
+sed -i -e "s|^PATHS=.*$|PATHS=${FAKEROOT_LIB_DIR}|g" ${FAKEROOT}
 export TICHKSUM_TOOL="tichksum"
 export TICHKSUM="${TOOLS_DIR}/${TICHKSUM_TOOL}"
 #export TICHKSUM="$TOOLS_DIR/TI-chksum-0.1/tichksum"
