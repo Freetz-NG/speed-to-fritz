92c68b908f3c7ec920f942c233b4ab139715cbb9
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-09-09 14:26:04 +0000

    20090909-1053 - Remove W920 copy of speed-to-fritz because it was not used.
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@525 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/W920/speed-to-fritz/alien/subscripts/swap_infoled_501.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#swap info led 0-4 with wlan led  
-sed -i -e 's|DEF wlan,0 = 0,9,4,wlan|DEF wlan,0 = 99,32,17,wlan|' "${1}/etc/led.conf"
-sed -i -e 's|DEF info,0 = 99,32,17,info|DEF info,0 = 0,9,4,info|' "${1}/etc/led.conf"
-sed -i -e 's|DEF info,1 = 99,32,17,info|DEF info,1 = 0,9,4,info|' "${1}/etc/led.conf"
-sed -i -e 's|DEF info,2 = 99,32,17,info|DEF info,2 = 0,9,4,info|' "${1}/etc/led.conf"
-sed -i -e 's|DEF info,3 = 99,32,17,info|DEF info,3 = 0,9,4,info|' "${1}/etc/led.conf"
-sed -i -e 's|DEF info,4 = 99,32,17,info|DEF info,4 = 0,9,4,info|' "${1}/etc/led.conf"
-exit 0
