96308b926d522dafe6659a9d9ef0bddc2526569b
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-09-14 20:47:03 +0000

    20090914-1659 - W920: fix dect problem, some text corrected
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@538 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/alien/subscripts/add_vdslprofilepage.sh
+++ trunk/speed-to-fritz/alien/subscripts/add_vdslprofilepage.sh
@@ -31,4 +31,15 @@ EOF
  fi
 fi
 
+sed -i -e '/uiPostAutodetect/d'  "${SRC}/usr/www/${OEMLINK}/html/de/wlan/wds.js"
+sed -i -e '/uiPostAutodetect/d'  "${SRC}/usr/www/${OEMLINK}/html/de/wlan/wds.frm"
+echo '<!--input type="hidden" name="sar:settings/vlan_encap" value="<? query sar:settings/vlan_encap ?>" id="uiPostVlanAktiv" disabled-->' >> "${SRC}/usr/www/${OEMLINK}/html/de/first/first_ISP_3.frm"
+echo '<input type="hidden" name="sar:settings/vlan_id" value="<? query sar:settings/vlan_id ?>" id="uiPostVlanId" disabled>' >> "${SRC}/usr/www/${OEMLINK}/html/de/first/first_ISP_3.frm"
+
+sed -i -e '/uiPostAnnex/a\
+<Input type="hidden" name="sar:settings/DSLMode" value="<? query sar:settings/DSLMode ?>" id="uiPostDSLMode" disabled>' "${SRC}/usr/www/${OEMLINK}/html/de/internet/dslsnrset.frm"
+sed -i -e '/uiPostSarDns1/a\
+<!--input type="hidden" name="sar:settings/vlan_encap" value="<? query sar:settings/vlan_encap ?>" id="uiPostVlanAktiv" disabled>\
+<input type="hidden" name="sar:settings/vlan_id" value="<? query sar:settings/vlan_id ?>" id="uiPostVlanId" disabled-->' "${SRC}/usr/www/${OEMLINK}/html/de/internet/internet_expert.frm"
+
 exit 0
