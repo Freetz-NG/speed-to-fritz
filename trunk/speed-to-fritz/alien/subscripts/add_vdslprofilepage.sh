d1b785341a36b1c2e0b8c359959c9c3a39835004
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-09-17 11:59:14 +0000

    20090917-0758 - refix DSL labor dslsnrset.frm is not present so dont patch if file is not existant.
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@552 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/alien/subscripts/add_vdslprofilepage.sh
+++ trunk/speed-to-fritz/alien/subscripts/add_vdslprofilepage.sh
@@ -30,18 +30,16 @@ EOF
   [ "$avm_Lang" != "de" ] && modpatch "${SRC}/${USRWWW}" "$P_DIR/add_vdslprofile_en.patch"
  fi
 fi
-
-sed -i -e '/uiPostAutodetect/d'  "${SRC}/usr/www/${OEMLINK}/html/de/wlan/wds.js"
-sed -i -e '/uiPostAutodetect/d'  "${SRC}/usr/www/${OEMLINK}/html/de/wlan/wds.frm"
+[ -f "${SRC}/usr/www/${OEMLINK}/html/de/wlan/wds.js" ] && sed -i -e '/uiPostAutodetect/d'  "${SRC}/usr/www/${OEMLINK}/html/de/wlan/wds.js"
+[ -f "${SRC}/usr/www/${OEMLINK}/html/de/wlan/wds.frm" ] && sed -i -e '/uiPostAutodetect/d'  "${SRC}/usr/www/${OEMLINK}/html/de/wlan/wds.frm"
 echo '<!--input type="hidden" name="sar:settings/vlan_encap" value="<? query sar:settings/vlan_encap ?>" id="uiPostVlanAktiv" disabled-->' >> "${SRC}/usr/www/${OEMLINK}/html/de/first/first_ISP_3.frm"
 echo '<input type="hidden" name="sar:settings/vlan_id" value="<? query sar:settings/vlan_id ?>" id="uiPostVlanId" disabled>' >> "${SRC}/usr/www/${OEMLINK}/html/de/first/first_ISP_3.frm"
-
-sed -i -e '/uiPostAnnex/a\
+[ -f "${SRC}/usr/www/${OEMLINK}/html/de/internet/dslsnrset.frm" ] && sed -i -e '/uiPostAnnex/a\
 <Input type="hidden" name="sar:settings/DSLMode" value="<? query sar:settings/DSLMode ?>" id="uiPostDSLMode" disabled>' "${SRC}/usr/www/${OEMLINK}/html/de/internet/dslsnrset.frm"
-sed -i -e '/uiPostSarDns1/a\
+[ -f "${SRC}/usr/www/${OEMLINK}/html/de/internet/internet_expert.frm" ] && sed -i -e '/uiPostSarDns1/a\
 <!--input type="hidden" name="sar:settings/vlan_encap" value="<? query sar:settings/vlan_encap ?>" id="uiPostVlanAktiv" disabled>\
 <input type="hidden" name="sar:settings/vlan_id" value="<? query sar:settings/vlan_id ?>" id="uiPostVlanId" disabled-->' "${SRC}/usr/www/${OEMLINK}/html/de/internet/internet_expert.frm"
-sed -i -e '/InitSpeed();/a\
+[ -f "${SRC}/usr/www/${OEMLINK}/html/de/internet/kabelmodem.js" ] && sed -i -e '/InitSpeed();/a\
 InitVlan();' "${SRC}/usr/www/${OEMLINK}/html/de/internet/kabelmodem.js"
 exit 0
 
