6067521c701c93ad0ae21a8d627625c3057b7ab9
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-09-18 11:17:57 +0000

    20090918-0716 -  Reversed remove updatetab patch varabel, becaus it was turned off, by latly added changes    * added warnig if clear mtd3 mtd4 is selected bevor ftp transfere taks place
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@554 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/sp-to-fritz.sh
+++ trunk/speed-to-fritz/sp-to-fritz.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/sbin
 # Date of current version:
 # TODO: LC_ALL= LANG= LC_TIME= svn info . | awk '/^Last Changed Date: / {print $4}'
 #dont chang this line formwat is used in ./start to get script version into Firmware.conf
-Tag="17"; Monat="09"; Jahr="09"
+Tag="18"; Monat="09"; Jahr="09"
 export SKRIPT_DATE="$Tag.$Monat.$Jahr"
 export SKRIPT_DATE_ISO="$Jahr.$Monat.$Tag"
 export SKRIPT_REVISION="$Jahr$Monat$Tag"
@@ -1021,7 +1021,7 @@ if [ "$ORI" != "y" ]; then
  #remove signature
  [ "$DONT_REM_SIGNATUR" != "y" ] && $sh_DIR/rmv_signatur.sh "${SRC}"
  #remove autoupdate tab
- [ "$REM_AUTOUPDATETAB" = "y" ] && $sh_DIR/remove_autoupdatetab.sh "${SRC}"
+ [ "$DONT_REM_AUTOUPDATETAB" != "y" ] && $sh_DIR/remove_autoupdatetab.sh "${SRC}"
  # patch update pages 
  [ "$DONT_PATCH_TOOLS" != "y" ] && $sh_DIR/patch_tools.sh "${SRC}"
  # update modules dependencies
