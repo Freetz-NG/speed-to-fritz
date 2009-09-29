f992554256662788706ee0845704b73302e256bd
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-09-29 22:10:17 +0000

    20090929-1806 - W721:  See: http://www.ip-phone-forum.de/showpost.php?p=1405007&postcount=411, Option added to set VLAN7
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@584 fc5b469a-3574-4ed2-a3ea-762ab7856fab

---
+++ trunk/speed-to-fritz/alien/subscripts/add_vlan_in_ar7.cfg.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# include modpatch function
+. ${include_modpatch}
+cp -fv "${SRC}/etc/default.${CONFIG_PRODUKT}/tcom/ar7.cfg" "${SRC}/etc/default.${CONFIG_PRODUKT}/avm/ar7.cfg"
+rpl_vlan()
+ {
+	for file in $1; do
+	if [ -f "$file" ]; then
+	sed -i -e '/ppptarget = "internet";/,/}/{s/pppoevlanauto =.*;/pppoevlanauto = yes;/}' "$file" 
+	sed -i -e '/ppptarget = "internet";/,/}/{s/pppoevlanauto_startwithvlan =.*;/pppoevlanauto_startwithvlan = yes;/}' "$file" 
+	sed -i -e '/dsl_encap = dslencap_mixed;/,/}/{s/vlanencap =.*;/vlanencap = vlanencap_class_prio;/}' "$file" 
+	sed -i -e '/dsl_encap = dslencap_mixed;/,/}/{s/vlanid =.*;/vlanid = 10;/}' "$file" 
+	sed -i -e '/dsl_encap = dslencap_mixed;/,/}/{s/vlanprio =.*;/vlanprio = 1;/}' "$file" 
+#	sed -i -e '/dsl_encap = dslencap_mixed;/,/}/{s/vlanencap =.*;/vlanencap = vlanencap_tcom;/}' "$file" 
+#	sed -i -e '/vlanencap_tcom/,/}/{s/vlanid =.*;/vlanid = 7;/}' "$file" 
+
+	grep -q 'vlanencap = vlanencap_class_prio;' "$file" && echo "-- Set vlan in ${file##*etc}..."
+	grep -q 'vlanencap = vlanencap_class_prio;' "$file" && echo2 "  -- set vlanencap = vlanencap_class_prio;"
+	grep -q 'vlanid = 10' "$file" && echo2 "  -- set vlanid = 10"
+	grep -q 'vlanprio = 1' "$file" && echo2 "  -- set vlanprio = 1"
+	grep -q 'pppoevlanauto = yes' "$file" && echo2 "  -- set pppoevlanauto = yes"
+	grep -q 'pppoevlanauto_startwithvlan = yes' "$file" && echo2 "  -- set pppoevlanauto_startwithvlan = yes"
+	grep -q 'vlanencap = vlanencap_tcom' "$file" && echo2 "  -- set  vlanencap = vlanencap_tcom"
+	grep -q 'vlanid = 7;' "$file" && echo2 "  -- set  vlanid = 7"
+	fi 
+	done
+ }
+
+rpl_vlan "$(find "${SRC}/etc" -name ar7.cfg)" 
