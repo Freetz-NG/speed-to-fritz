c5a328c1cde15c3bc9d1aa866382ded3be51f985
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-10-30 09:00:51 +0000

    - update.sh: Can be used to update to 'Ubuntu 9.10 karmic koala' now.
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@627 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/co/henrynestler/update.sh
+++ trunk/co/henrynestler/update.sh
@@ -2,10 +2,15 @@
 
 sudo apt-get update
 sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
+sudo apt-get clean -y
 sleep 10
-sudo sed -i -e 's|gutsy|hardy|' "/etc/apt/sources.list"
+#sudo sed -i -e 's|gutsy|hardy|' "/etc/apt/sources.list"
 #sudo sed -i -e 's|hardy|intrepid|' "/etc/apt/sources.list"
 #sudo sed -i -e 's|intrepid|jaunty|' "/etc/apt/sources.list"
+#Karmic Koala
+sudo sed -i -e 's|jaunty|karmic|' "/etc/apt/sources.list"
 sleep 10
 sudo apt-get update
 sudo apt-get dist-upgrade -y
@@ -14,6 +19,7 @@ sudo apt-get autoclean -y
 sudo apt-get clean -y
 
 exit
+
 apt-get install update-manager-core
 do-release-upgrade -d #ohne -d 
 
