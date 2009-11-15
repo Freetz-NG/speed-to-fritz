e4bfbcfbd6cd16a193b9b850dbcc5ae0f93181cb
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-11-15 18:01:34 +0000

    20091115-1300 - multilingual: fix tam and fax files, some more files are included, default language can now be an other language, but this is not recommanded, for some text no fallback language is then avalabel!
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@665 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/alien/subscripts/add_multi_settings.sh
+++ trunk/speed-to-fritz/alien/subscripts/add_multi_settings.sh
@@ -240,6 +240,15 @@ if [ "${FORCE_MULTI_LANGUAGE}" = "y" ]; then
     if [ -d "${SRC_2}/usr/share/tam/msg/default/$DIR" ]; then
      cp -fdrp "${SRC_2}/usr/share/tam/msg/default/$DIR" "${SRC}/usr/share/tam/msg/default/$DIR" && echo2 "  Copy TAM language directory from 2nd AVM firmware: $DIR"
     fi 
+    FileList="/usr/share/telefon/tam-$DIR.html /usr/share/telefon/fax-$DIR.html /usr/share/telefon/tam-$DIR.txt /usr/share/telefon/fax-$DIR.txt"
+    for Langufile in $FileList; do
+     if [ -f "${DST}/$Langufile" ]; then
+      cp -f "${DST}/$Langufile" "${SRC}/$Langufile" && echo2 "  Copy: $Langufile"
+     fi 
+     if [ -d "${SRC_2}/$Langufile" ]; then
+      cp -f "${SRC_2}/$Langufile" "${SRC}/$Langufile" && echo2 "  Copy $Langufile"
+     fi 
+    done
    done
    LanguageList="en it es fr de"
    for lang in $LanguageList; do
@@ -250,12 +259,19 @@ if [ "${FORCE_MULTI_LANGUAGE}" = "y" ]; then
      echo -e "-- \033[1mWarning:\033[0m t-Home firmware database $lang is used, some text may be missing." && sleep 2
     fi
    done
+   FileList="root_ca.pem root_ca_ta.pem root_ca_mnet.pem"
+    for ca_file in $FileList; do
+     [ -f "${DST}/etc/default.${DEST_PRODUKT}/${OEML}/$ca_file" ] && cp -f "${DST}/etc/default.${DEST_PRODUKT}/${OEML}/$ca_file" "${SRC}/etc/default.${CONFIG_PRODUKT}/${OEMLINK}/$ca_file" && echo2 "  Copy: $ca_file"
+     [ -f "${SRC_2}/etc/default.${SORCE_2_PRODUKT}/${OEML2}/$ca_file" ] && cp -f "${SRC_2}/etc/default.${SORCE_2_PRODUKT}/${OEML2}/$ca_file" "${SRC}/etc/default.${CONFIG_PRODUKT}/${OEMLINK}/$ca_file" && echo2 "  Copy: $ca_file"
+   done 
+
 fi # <-- multilanguage
 if [ "${FORCE_LANGUAGE}" != "de" ]; then
    #[ -f "${SRC}"/etc/htmltext_de.db ] || echo -e "-- \033[1mAttention:\033[0m 1st Firmware is not usabel for force language!" && sleep 7
-   [ "${FORCE_LANGUAGE}" != "" ] && [ -f "${DST}"/etc/htmltext_${FORCE_LANGUAGE}.db ] && cp -fdrp "${DST}"/etc/htmltext_${FORCE_LANGUAGE}.db --target-directory="${SRC}"/etc && echo2 "  copy: database ${FORCE_LANGUAGE}"
-   [ "${FORCE_LANGUAGE}" != "" ] && [ -f "${SRC_2}"/etc/htmltext_${FORCE_LANGUAGE}.db ] && cp -fdrp "${SRC_2}"/etc/htmltext_${FORCE_LANGUAGE}.db --target-directory="${SRC}"/etc && echo2 "  copy: database ${FORCE_LANGUAGE}"
+   [ "${FORCE_LANGUAGE}" != "" ] && [ -f "${DST}"/etc/htmltext_${FORCE_LANGUAGE}.db ] && cp -fdrp "${DST}"/etc/htmltext_${FORCE_LANGUAGE}.db --target-directory="${SRC}"/etc && echo2 "  Copy: database ${FORCE_LANGUAGE}"
+   [ "${FORCE_LANGUAGE}" != "" ] && [ -f "${SRC_2}"/etc/htmltext_${FORCE_LANGUAGE}.db ] && cp -fdrp "${SRC_2}"/etc/htmltext_${FORCE_LANGUAGE}.db --target-directory="${SRC}"/etc && echo2 "  Copy: database ${FORCE_LANGUAGE}"
 fi
+
 if [ "${REMOVE_SOME_LANGUAGE}" = "y" ]; then
     [ "${REMOVE_EN}" = "y" ] && LanguageList="en "
     [ "${REMOVE_DE}" = "y" ] && LanguageList+="de "
@@ -263,11 +279,20 @@ if [ "${REMOVE_SOME_LANGUAGE}" = "y" ]; then
     [ "${REMOVE_ES}" = "y" ] && LanguageList+="es "
     [ "${REMOVE_FR}" = "y" ] && LanguageList+="fr "
    for DIR in $LanguageList; do
-    if [ "${DIR}" != "$avm_Lang" ]; then
-     [ -d "${SRC}/etc/default.${DEST_PRODUKT}"/${OEM}/$DIR ] && rm -fdR "${SRC}/etc/default.${CONFIG_PRODUKT}/${OEM}/$DIR" && echo2 "  Removed language directory: $DIR"
-     [ -f "${SRC}/etc/htmltext_$DIR.db" ] && rm -fr "${SRC}/etc/htmltext_$DIR.db" && echo -e "-- Language database $DIR removed."
-     [ -d "${SRC}/usr/share/tam/msg/default/$DIR" ] && rm -fdR "${SRC}/usr/share/tam/msg/default/$DIR" && echo2 "  Removed TAM language directory: $DIR"
+    if [ "${DIR}" != "$FORCE_LANGUAGE" ]; then
+     rm -fdR "${SRC}/etc/default.${CONFIG_PRODUKT}/${OEM}/$DIR" && echo2 "  Removed language directory: $DIR"
+     rm -fr "${SRC}/etc/htmltext_$DIR.db" && echo -e "-- Language database $DIR removed."
+     rm -fdR "${SRC}/usr/share/tam/msg/default/$DIR" && echo2 "  Removed TAM language directory: $DIR"
+     FileList="/usr/share/telefon/tam-$DIR.html /usr/share/telefon/fax-$DIR.html /usr/share/telefon/tam-$DIR.txt /usr/share/telefon/fax-$DIR.txt"
+     for Langufile in $FileList; do
+       rm -f "${SRC}/$Langufile" && echo2 "  Removed: $Langufile"
+     done
     fi
    done
+fi
+#set default datadase link if de is not avalabel
+if ! [ -f "${SRC}/etc/htmltext_de.db" ] ; then
+    [ -L "${SRC}/etc/htmltext.db" ] && rm -fd -R "${SRC}/etc/htmltext.db"
+    ln -s /etc/htmltext_$FORCE_LANGUAGE.db  "${SRC}/etc/htmltext.db"
 fi
 exit 0
