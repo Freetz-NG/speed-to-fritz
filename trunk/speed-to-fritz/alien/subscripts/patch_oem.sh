6426f8c1e0d8e17697d3f1d81874b28bb338041a
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2009-12-03 21:57:30 +0000

    20091203-1656 - Add Option build recover for original tcom firmware.
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@705 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/alien/subscripts/patch_oem.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- . $include_modpatch
-	sed -i -e "/export CONFIG_ENVIRONMENT_PATH=\/proc\/sys\/urlader/a \
-	echo firmware_version ${OEM} > \$CONFIG_ENVIRONMENT_PATH\/environment \
-	echo my_ipaddress 192.168.178.1 > \$CONFIG_ENVIRONMENT_PATH\/environment" "${1}/etc/init.d/rc.S"
-	grep -q "echo firmware_version ${OEM} >" "${1}/etc/init.d/rc.S" && echo "-- OEM set via Firmware to: $OEM" 
-	grep -q "my_ipaddress 192.168.178.1 >" "${1}/etc/init.d/rc.S" && echo "-- OEM set via Firmware to: $OEM" 
- exit 0
