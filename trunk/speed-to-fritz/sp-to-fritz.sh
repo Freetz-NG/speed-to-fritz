a1894c459fc77f3cb7963ae63b1eb269509853af
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2010-03-07 10:41:20 +0000

    20100307-0540 - *  Fix phonelabor 16624 reverse lookup patch, not tesetd!
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@824 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/sp-to-fritz.sh
+++ trunk/speed-to-fritz/sp-to-fritz.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/sbin
 # Date of current version:
 # TODO: LC_ALL= LANG= LC_TIME= svn info . | awk '/^Last Changed Date: / {print $4}'
 #dont chang this line formwat is used in ./start to get script version into Firmware.conf
-Tag="06"; Monat="03"; Jahr="10"
+Tag="07"; Monat="03"; Jahr="10"
 export SKRIPT_DATE="$Tag.$Monat.$Jahr"
 export SKRIPT_DATE_ISO="$Jahr.$Monat.$Tag"
 export SKRIPT_REVISION="$Jahr$Monat$Tag"
