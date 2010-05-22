9795385851a85ff87c71b80c98295e0affc1152b
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2010-05-22 15:24:15 +0000

    20100522-1123 - Added option 'Add provider AON' is in us in austria. and aditional cleanups without real source changes.
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@913 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/alien/subscripts/add_provider_aon.sh
+++ trunk/speed-to-fritz/alien/subscripts/add_provider_aon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # include modpatch function
 . ${include_modpatch}
-FILELIST="/html/de/first/provider.js"
+FILELIST="/html/de/first/provider.js /html/de/internet/authform.html"
 
 for FILE in $FILELIST; do
   if [ -f "${SRC}/usr/www/${OEMLINK}/$FILE" ]; then 
@@ -10,7 +10,9 @@ for FILE in $FILELIST; do
 if(szProvider== "AON") return szProvider;' "${SRC}/usr/www/${OEMLINK}/$FILE"
 	sed -i -e '/return otherprovider;/i\
 if (id == "AON") return "aon";' "${SRC}/usr/www/${OEMLINK}/$FILE"
-    grep -q "AON" "${SRC}/usr/www/${OEMLINK}/$FILE" && echo "-- Added provider AON" 
+	sed -i -e '/<option value="Inode">/i\
+<option value="AON">AON Telekom Austria</option>' "${SRC}/usr/www/${OEMLINK}/$FILE"
+    grep -q "AON" "${SRC}/usr/www/${OEMLINK}/$FILE" && echo "-- Added provider AON to: $FILE" 
     fi
   fi
 done
--- trunk/speed-to-fritz/alien/subscripts/set_allproviders.sh
+++ trunk/speed-to-fritz/alien/subscripts/set_allproviders.sh
@@ -8,8 +8,8 @@ FILELIST="/html/de/internet/authform.html"
 for FILE in $FILELIST; do
   if [ -f "${SRC}/usr/www/${OEMLINK}/$FILE" ]; then 
     sed -i -e 's/$var:allprovider 1/ 1 1/' "${SRC}/usr/www/${OEMLINK}/$FILE"
+    sed -i -e 's/$var:OEM avme/avme avme/' "${SRC}/usr/www/${OEMLINK}/$FILE"
     echo2 "  /usr/www/${OEMLINK}/$FILE"
   fi
 done
-
 exit 0
