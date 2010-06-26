10e5b30882efca122bb89f068ed495189b96aa41
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2010-06-26 15:18:59 +0000

    20100626-1118 - Update header for 17675
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@956 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/alien/subscripts/rpl_header.sh
+++ trunk/speed-to-fritz/alien/subscripts/rpl_header.sh
@@ -10,6 +10,9 @@ for DIR in ${OEMLIST}; do
 	 echo "-- Added header: /usr/www/${HTML}/de/images/fw_header980.gif"
 	[ -f $P_DIR/fw_header.gif ] && [ -f "$1"/usr/www/${HTML}/de/images/fw_header.gif ] && cp -fdpr  $P_DIR/fw_header.gif  --target-directory="$1"/usr/www/${HTML}/de/images &&\
 	echo "-- Added header: /usr/www/${HTML}/de/images/fw_header.gif"
+	# 17675
+	[ -f $P_DIR/kopfbalken_mitte.gif ] && [ -f "$1"/usr/www/$DIR/css/default/images/kopfbalken_mitte.gif ] && cp -fdpr  $P_DIR/kopfbalken_mitte.gif  --target-directory="$1"/usr/www/$DIR/css/default/images &&\
+	echo "-- Added header: /usr/www/$DIR/css/default/images/kopfbalken_mitte.gif"
     fi
 done
 exit 0
