39d6d9975cf639c88624b55b28363244084fb257
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2010-10-16 11:07:08 +0000

    20101016-0706 -   * Fix add VDSL profile page option, not tested.
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@1120 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/alien/subscripts/add_vdslprofilepage.sh
+++ trunk/speed-to-fritz/alien/subscripts/add_vdslprofilepage.sh
@@ -4,14 +4,9 @@
 if [ "${ADD_VDSL_PROFILE}" = "y" ]; then
  echo "-- adding vdsl profile pages ..."
 
-#  for file_n in /html/de//internet/feedback.js /html/de/internet/feedback.html /html/de/internet/feedback.frm\
-# /html/de/home/feedback.js /html/de/home/feedback.html /html/de/home/feedback.frm; do
-#    rm -f "${SRC}/usr/www/${OEMLINK}/$file_n" && echo2 "  -- removed file: $file_n"
-#  done
   for file_n in atm.html adsl.html bits.html overview.html; do
   sed -i -e '/uiDoSNRPage()/a\
 <li><a href="javascript:uiDoVdslProfilePage()">Vdsl Profile</a></li>'  "${SRC}/usr/www/${OEMLINK}/html/de/internet/$file_n"
-  sed -i -e 's/uiDoFeedbackPage()/uiDoVdslProfilePage/'  "${SRC}/usr/www/${OEMLINK}/html/de/internet/$file_n"
   done 
 
 cat >> "${SRC}/usr/www/${OEMLINK}/html/de/internet/awatch.js" << EOF
@@ -19,7 +14,6 @@ function uiDoVdslProfilePage() {
 jslGoTo("internet", "vdsl_profile");
 }
 EOF
-
  if ! [ -f "${SRC}/usr/www/${OEMLINK}/html/de/internet/vdsl_profile.html" ]; then
   for file_n in vdsl_profile.html vdsl_profile.js vdsl_profile.frm; do
    [ -f "${SRC}/usr/www/${OEMLINK}/html/de/internet/$file_n" ] && rm -f "${SRC}/usr/www/${OEMLINK}/html/de/internet/$file_n"
