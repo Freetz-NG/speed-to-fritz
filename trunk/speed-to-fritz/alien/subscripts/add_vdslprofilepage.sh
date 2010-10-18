97453dbce4c4970becd8d98d61e9ff71c5786ca9
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2010-10-18 21:17:07 +0000

    20101018-1716 -   * fix last commit
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@1129 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/alien/subscripts/add_vdslprofilepage.sh
+++ trunk/speed-to-fritz/alien/subscripts/add_vdslprofilepage.sh
@@ -3,36 +3,40 @@
 . ${include_modpatch}
 if [ "${ADD_VDSL_PROFILE}" = "y" ]; then
  echo "-- adding vdsl profile pages ..."
+ DIRI="$(find ${1}/usr/www/ \( -name adsl.html -o -name dslsnrset.html -o -name atm.html -o -name bits.html -o -name overview.html -o -name feedback.html -o -name vdsl_profile.html \) -type f -print)"
+ #-----------------------------------------------------------------
+ for file_n in $DIRI; do
+    [ "${file_n##*/}" == "bits.html" ] &&\
+    sed -i -e 's|<li class="tabs_on"><div class="activtab">.*</div></li>|<li class="tabs_on"><div class="activtab">Vdsl Profile</div></li>|g' "$file_n"
+  sed -i -e '/uiDoBitsPage()/a\
+<li><a href="javascript:uiDoVdslProfilePage()">Vdsl Profile</a></li>'  "$file_n"
+  sed -i -e "/uiDoFeedbackPage()/d" "$file_n"
+    grep -q "Vdsl Profile" "$file_n" && echo2 "    added tab 'Vdsl Profile' in file:${file_n##*/}"
+ done 
 
-  for file_n in atm.html adsl.html bits.html overview.html; do
-  sed -i -e '/uiDoSNRPage()/a\
-<li><a href="javascript:uiDoVdslProfilePage()">Vdsl Profile</a></li>'  "${SRC}/usr/www/${OEMLINK}/html/de/internet/$file_n"
-  done 
-
-cat >> "${SRC}/usr/www/${OEMLINK}/html/de/internet/awatch.js" << EOF
+cat >> "${1}/usr/www/${OEMLINK}/html/de/internet/awatch.js" << EOF
 function uiDoVdslProfilePage() {
 jslGoTo("internet", "vdsl_profile");
 }
 EOF
- if ! [ -f "${SRC}/usr/www/${OEMLINK}/html/de/internet/vdsl_profile.html" ]; then
+ if ! [ -f "${1}/usr/www/${OEMLINK}/html/de/internet/vdsl_profile.html" ]; then
   for file_n in vdsl_profile.html vdsl_profile.js vdsl_profile.frm; do
-   [ -f "${SRC}/usr/www/${OEMLINK}/html/de/internet/$file_n" ] && rm -f "${SRC}/usr/www/${OEMLINK}/html/de/internet/$file_n"
+   [ -f "${1}/usr/www/${OEMLINK}/html/de/internet/$file_n" ] && rm -f "${1}/usr/www/${OEMLINK}/html/de/internet/$file_n"
   done
-  touch "${SRC}/usr/www/${OEMLINK}/html/de/internet/vdsl_profile.frm"
+  touch "${1}/usr/www/${OEMLINK}/html/de/internet/vdsl_profile.frm"
   USRWWW="usr/www/${OEMLINK}/html/de"
-  [ "$avm_Lang" = "de" ] && modpatch "${SRC}/${USRWWW}" "$P_DIR/add_vdslprofile_de.patch"
-  [ "$avm_Lang" != "de" ] && modpatch "${SRC}/${USRWWW}" "$P_DIR/add_vdslprofile_en.patch"
+  [ "$avm_Lang" = "de" ] && modpatch "${1}/${USRWWW}" "$P_DIR/add_vdslprofile_de.patch"
+  [ "$avm_Lang" != "de" ] && modpatch "${1}/${USRWWW}" "$P_DIR/add_vdslprofile_en.patch"
  fi
-fi
 
 sed -i -e '/Trellis="on"/i\
 local profil_conf=`ctlmgr_ctl r sar settings/UsNoiseBits`\
 local trellis_conf=`ctlmgr_ctl r sar settings/RFI_mode`\
-' "${SRC}/etc/init.d/rc.vdsl.sh"
+' "${1}/etc/init.d/rc.vdsl.sh"
 
-sed -i -e 's|#Profile="30A"|[ $profil_conf == "1" ] \&\& Profile="30A"|' "${SRC}/etc/init.d/rc.vdsl.sh"
-sed -i -e 's|#Trellis="off"|[ $trellis_conf == "1" ] \&\& Trellis="off"|' "${SRC}/etc/init.d/rc.vdsl.sh"
-
+sed -i -e 's|#Profile="30A"|[ $profil_conf == "1" ] \&\& Profile="30A"|' "${1}/etc/init.d/rc.vdsl.sh"
+sed -i -e 's|#Trellis="off"|[ $trellis_conf == "1" ] \&\& Trellis="off"|' "${1}/etc/init.d/rc.vdsl.sh"
+fi
 
 exit 0
 
