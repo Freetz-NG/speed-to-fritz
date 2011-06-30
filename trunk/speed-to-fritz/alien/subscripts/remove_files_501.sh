c4b81ce5a6e8571d8da57e4da90fe1351fe35965
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2011-06-30 17:20:39 +0000

    * Remove some more files on boxes without USB
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@1333 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/alien/subscripts/remove_files_501.sh
+++ trunk/speed-to-fritz/alien/subscripts/remove_files_501.sh
@@ -1,120 +1,75 @@
 #!/bin/bash
- . ${include_modpatch}
+. ${include_modpatch}
 echo2 "  -- removing files ..."
 for DIR in ${OEMLISt}; do
-# if [ "$DIR" = "avme" ] ; then
-#  export HTML="$DIR/$avm_Lang/html"
-# else
   export HTML="$DIR/html"
-# fi
     DSTI="${1}"/usr/www/$HTML
     if [ -d ${DSTI} ]; then
 #-----------------------------------------
 echo "-- /usr/www/$HTML/$avm_Lang/..."
-
-
-rm -fd -R ${1}/usr/www/$HTML/$avm_Lang/software
-rm -fd -R ${1}/usr/www/$HTML/$avm_Lang/usb
-rm -fd -R ${1}/usr/www/$HTML/$avm_Lang/tr69_autoconfig
-rm -fd -R ${1}/usr/www/$HTML/$avm_Lang/dect
-
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/menus/menu2_usb*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/menus/menu2_usb.inc
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/menus/menu2_software*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/menus/menu2_tr69*
-
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/home/fon1Dect*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/home/usb*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/home/fon1tam*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/home/tam*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/home/fonab*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/home/isdn*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/home/fonlistisdn*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/home/fon1isdn*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/home/buchsend*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/fon/fon1Dect*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/fon/usb*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/fon/fon1tam*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/fon/tam*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/fon/fonab*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/fon/isdn*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/fon/fonlistisdn*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/fon/fon1isdn*
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/fon/buchsend*
-
-#rm -rf  ${1}/usr/www/$HTML/$avm_Lang/fon/mini*
-#rm -rf  ${1}/usr/www/$HTML/$avm_Lang/home/mini*
-#rm -rf  ${1}/usr/www/$HTML/$avm_Lang/home/fon1mini*
-#rm -rf  ${1}/usr/www/$HTML/$avm_Lang/fon/fon1mini*
-
-rm -rf  ${1}/usr/www/$HTML/$avm_Lang/fon_config/fon_config_Isdn*
-
-rm -rf  ${1}/usr/www/$HTML/tam.html
-
-
-
-
-#[ ${REMOVE_KIDS} = "y" ] && rm -fd -R ${1}/usr/www/kids
-#annexb firmware to big, remove more
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/software | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/usb | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/tr69_autoconfig | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/dect | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/menus/menu2_usb* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/menus/menu2_usb.inc | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/menus/menu2_software* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/menus/menu2_tr69* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/home/fon1Dect* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/home/usb* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/home/fon1tam* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/home/tam* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/home/fonab* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/home/isdn* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/home/fonlistisdn* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/home/fon1isdn* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/home/buchsend* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/fon/fon1Dect* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/fon/usb* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/fon/fon1tam* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/fon/tam* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/fon/fonab* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/fon/isdn* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/fon/fonlistisdn* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/fon/fon1isdn* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/fon/buchsend* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/fon_config/fon_config_Isdn* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/tam.html | sed "s/.*\//\t'/"
 if [ $TCOM_V_MINOR -eq 38 ] && [ $AVM_V_MINOR -eq 33 ]; then
-
-rm -fd -R ${1}/usr/www/$HTML/$avm_Lang/tools
-rm -fd -R ${1}/usr/www/$HTML/$avm_Lang/first
-#rm -fd -R ${1}/usr/www/$HTML/$avm_Lang/fon_config
-rm -fd -R ${1}/usr/www/kids
-rm -fd -R ${1}/usr/www/$HTML/tools
-
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/tools | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/$avm_Lang/first | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/kids | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/www/$HTML/tools | sed "s/.*\//\t'/"
 fi
 #---------------------------------------------
     fi
-
-#[ ${OEM} != ${DIR} ] && echo "-- Remove /etc/default.${CONFIG_PRODUKT}/$DIR"
-[ ${OEM} != ${DIR} ] && rm -fd -R ${1}/etc/default.${CONFIG_PRODUKT}/$DIR
-
+[ ${OEM} != ${DIR} ] && rm -rf $VERBOSE ${1}/etc/default.${CONFIG_PRODUKT}/$DIR
 done
-
-
-rm -rf ${1}/sbin/wlan_cal
-rm -rf ${1}/sbin/tracking
-rm -rf ${1}/sbin/finalize_env
-rm -rf ${1}/sbin/printserv
-rm -rf ${1}/sbin/hotplug
-
-rm -rf ${1}/bin/usbhostchange
-
-rm -rf ${1}/usr/share/ctlmgr/libctlusb*
-rm -rf ${1}/usr/share/ctlmgr/libtr069*
-rm -rf ${1}/usr/share/ctlmgr/libdect*
-rm -rf ${1}/usr/share/ctlmgr/libtam*
-rm -rf ${1}/usr/share/telefon/libtam*
-rm -rf ${1}/usr/share/telefon/tam*
-
-#rm -rf ${1}/usr/share/telefon/libmini*
-
-
-rm -rf -R ${1}/usr/share/tam
-
-rm -rf ${1}/usr/bin/tr069fwupdate
-
-rm -rf ${1}/etc/default.049/fx_conf.default.1
-rm -rf ${1}/etc/default.049/fx_lcr.default.1
-rm -rf ${1}/etc/default.049/fx_lcr.aol
-
-rm -rf ${1}/etc/usbclass.tab  
-rm -rf ${1}/etc/usbdevice.tab  
-
-rm -fd -R ${1}/etc/hotplug
-
-rm -rf  ${1}/lib/modules/2.6.13.1-ohio/modules.usbmap
-
-rm -fd -R ${1}/lib/modules/2.6.13.1-ohio/kernel/drivers/scsi
-rm -fd -R ${1}/lib/modules/2.6.13.1-ohio/kernel/drivers/usb
-rm -fd -R ${1}/lib/modules/2.6.13.1-ohio/kernel/drivers/cdrom
-rm -fd -R ${1}/lib/modules/2.6.13.1-ohio/kernel/drivers/char/audio
-rm -fd -R ${1}/lib/modules/2.6.13.1-ohio/kernel/drivers/char/flash_update
-rm -fd -R ${1}/lib/modules/2.6.13.1-ohio/kernel/fs
-
-
-
-
+rm -rf $VERBOSE ${1}/sbin/wlan_cal | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/sbin/tracking | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/sbin/finalize_env | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/sbin/printserv | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/sbin/hotplug | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/bin/usbhostchange | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/share/ctlmgr/libctlusb* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/share/ctlmgr/libtr069* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/share/ctlmgr/libdect* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/share/ctlmgr/libtam* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/share/telefon/libtam* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/share/telefon/tam* | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/share/tam | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/usr/bin/tr069fwupdate | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/etc/default.049/fx_conf.default.1 | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/etc/default.049/fx_lcr.default.1 | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/etc/default.049/fx_lcr.aol | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/etc/usbclass.tab | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/etc/usbdevice.tab | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/etc/hotplug | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/lib/modules/2.6.13.1-ohio/modules.usbmap | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/lib/modules/2.6.13.1-ohio/kernel/drivers/scsi | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/lib/modules/2.6.13.1-ohio/kernel/drivers/usb | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/lib/modules/2.6.13.1-ohio/kernel/drivers/cdrom | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/lib/modules/2.6.13.1-ohio/kernel/drivers/char/audio | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/lib/modules/2.6.13.1-ohio/kernel/drivers/char/flash_update | sed "s/.*\//\t'/"
+rm -rf $VERBOSE ${1}/lib/modules/2.6.13.1-ohio/kernel/fs | sed "s/.*\//\t'/"
 exit 0
