7b5a6c2f9772803d24e7f1fe9e793ba81216861c
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2011-07-07 18:06:33 +0000

    * Add SP300 to download MT Firmware script, Download_MT_Firmware.sh
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@1348 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/Download_MT_Firmware.sh
+++ trunk/speed-to-fritz/Download_MT_Firmware.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 echo "!-------------------------------------------------------------------------!"
+#hw="01.01" #Fuer das MT-D
+#hw="02.01" #Fuer das SP300
+#hw="03.01" #Fuer das MT-F
+#hw="04.01" #Fuer das C3
 hw=
+sw=02.45
+oem=avm
+lang=de
+country=049
+fw=75.04.91.19965
+macaddr=0...
 while ! [ "$hw" ]; do
  KEY=
  while [ "$KEY" != "y" ]; do
@@ -26,40 +36,43 @@ while ! [ "$hw" ]; do
 	    echo -n "   C3 (y/n)? "; read -n 1 -s YESNO; echo
 	    [ "$YESNO" = "y" ] || [ "$YESNO" = "n" ] &&  KEY="y" || echo "wrong key!"
 	done
-	[ "$YESNO" = "y" ] && hw="04.01"
+	if [ "$YESNO" = "y" ]; then
+	    hw="04.01"
+	else
+	    KEY=
+	    while [ "$KEY" != "y" ]; do
+		echo
+		echo -n "   SP300 (y/n)? "; read -n 1 -s YESNO; echo
+		[ "$YESNO" = "y" ] || [ "$YESNO" = "n" ] &&  KEY="y" || echo "wrong key!"
+	    done
+		if [ "$YESNO" = "y" ]; then
+		    hw="02.01"
+		    sw=01.70
+	    	fi
+	fi
     fi
  fi
 done
+def=$sw,$oem,$country,$fw
 KEY=
 while [ "$KEY" != "y" ]; do
 	    echo
-	    echo -n "   Voreistellung (02.45,avm,de,049,75.04.91.19965) verwenden (y/n)? "; read -n 1 -s YESNO; echo
+	    echo -n "   Voreinstellung ($def) verwenden (y/n)? "; read -n 1 -s YESNO; echo
 	    [ "$YESNO" = "y" ] || [ "$YESNO" = "n" ] &&  KEY="y" || echo "wrong key!"
 done
-if [ "$YESNO" = "y" ]; then
-    sw=02.45
-    oem=avm
-    lang=de
-    country=049
-    fw=75.04.91.19965
-else
+if [ "$YESNO" != "y" ]; then
     IFS=','
     echo -e "Bitte fuenf Parameter, getrennt durch Komma eingeben:\n\
 02.45, Softwareversion derzeit am MT in Verwendung\n\
-avm, irgend ein gueltiger OEM zB: avme, avm, 1und1,...\n\
-de, Sprache zB: de, en\n\
-049, Landeskennug\n\
-75.04.91.19965, Firmwaeversion\n\
-Beispiel: 02.45,avm,de,049,75.04.91.19965\n"
+avm, ein gueltiger OEM zB: avme, avm, 1und1,...\n\
+de, Sprache zB: de, en, ...\n\
+049, Landeskennug zB: 049, 043, ...\n\
+75.04.91.19965, Box Firmwaeversion\n\
+Beispiel: $def\n"
     read sw oem country fw
 fi
 echo -e "Eingabe war: $sw $oem $country $fw\n\
 Firmware wird gesucht ...\n"
-#hw="01.01" #Fuer das MT-D
-#hw="03.01" #Fuer das MT-F
-#hw="04.01" #Fuer das C3
-lang=de
-macaddr=0...
 dladr="http://update.avm.de/cgi-bin/cati?hw=${hw}&sw=${sw}&oem=${oem}&lang=${lang}&country=${country}&fw=${fw}&macaddr=${macaddr}"
 #echo $dladr
 wget -nv $dladr --output-document=.url
