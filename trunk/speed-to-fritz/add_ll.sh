7da2a990de863aa9d2d140ecc246ef49db8cb939
jpascher <jpascher@fc5b469a-3574-4ed2-a3ea-762ab7856fab>
2011-08-22 07:57:34 +0000

    * fix http://www.ip-phone-forum.de/showthread.php?t=203800&p=1744506&viewfull=1#post1744506
    git-svn-id: https://svn.code.sf.net/p/freetzlinux/code@1401 fc5b469a-3574-4ed2-a3ea-762ab7856fab

--- trunk/speed-to-fritz/add_ll.sh
+++ trunk/speed-to-fritz/add_ll.sh
@@ -3,6 +3,7 @@ if ! [ -f "./link.lst" ]; then
 	cat ./Config.in > ./link.lst
 	sed -i -e '1,/--- General settings for Speed-to-fritz ---/ s/.*//' -e '/---     Config or Menu    ---/,$ s/.*//g' -e 's/#.*//' -e '/^$/d' "./link.lst"
 	sed -i -e 's/^.*default//' -e 's/^.*config *//' -e 's/^ *//' -e '/string/d' -e '/mainmenu/d' -e '/comment/d' -e 's|^"|="|' "./link.lst"
+	sed -i -e '/^$/ d' "./link.lst" #remove blanc lines
 	sed -i -e '$!N;s/\n//' "./link.lst" 	# join two lines
 fi
 if ! grep -q  '@AVM' ./Firmware.conf && ! grep -q  -e 'http://'  ./Firmware.conf ;then
